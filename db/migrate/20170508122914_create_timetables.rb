d87d97188f528c48e75cf933b7034da5f12df5e7
Fathi Abdul Rahim <fathiabdrahim@yahoo.com>
2017-05-12 23:59:48 +0800

    update timetable date to string

--- db/migrate/20170508122914_create_timetables.rb
+++ db/migrate/20170508122914_create_timetables.rb
@@ -1,15 +1,16 @@
 class CreateTimetables < ActiveRecord::Migration[5.1]
   def change
     create_table :timetables do |t|
-      t.integer :zone_id
-      t.date    :tarikh
-      t.string  :imsak
-      t.string  :subuh
-      t.string  :syuruk
-      t.string  :zohor
-      t.string  :asar
-      t.string  :maghrib
-      t.string  :isyak
+      t.integer   :zone_id
+      t.string    :tarikh
+      t.string    :imsak
+      t.string    :subuh
+      t.string    :syuruk
+      t.string    :zohor
+      t.string    :asar
+      t.string    :maghrib
+      t.string    :isyak
+      t.datetime  :serial
 
       t.timestamps
     end
