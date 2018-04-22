43e31b96678d5201b2c63202aefcf21c016e6585
Mohd Khairi Mohd Adnan <khairi@labs.my>
2018-04-22 10:24:58 +0800

    upgrade rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508122914) do
+ActiveRecord::Schema.define(version: 2017_05_08_122914) do
 
   create_table "locations", force: :cascade do |t|
     t.integer "zone_id"
