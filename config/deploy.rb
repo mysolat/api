b28fb88245b014d74fa405b817e85479a5181769
Mohd Khairi Mohd Adnan <khairi@labs.my>
2018-05-20 05:10:01 +0800

    update deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :forward_agent, true     # SSH forward_agent.
 # Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
-set :shared_dirs,  fetch(:shared_dirs, []).push('public/rails', 'storage')
+set :shared_dirs,  fetch(:shared_dirs, []).push('public/rails', 'storage', 'db/sqlite')
 set :shared_files, fetch(:shared_files, []).push('config/application.yml', 'config/database.yml')
 
 # This task is the environment that is loaded for all remote run commands, such as
